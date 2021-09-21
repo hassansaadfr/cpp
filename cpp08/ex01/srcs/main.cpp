e3106962912adccc8d03cdbe28662fe67def04b2
hassansaadfr <saadaoui.hassan@laposte.net>
2021-09-21 21:18:25 +0200

    done

--- cpp08/ex01/srcs/main.cpp
+++ cpp08/ex01/srcs/main.cpp
@@ -7,13 +7,50 @@ int	main(void)
 	srand(time(0));
 	std::list<int> lst;
 
-	for (size_t i = 0; i < 10; i++)
+	for (size_t i = 0; i < 10000; i++)
 		lst.push_back(rand() % 100000 + 1);
 
-	Span big = Span(10);
-	big.addNumber(lst.begin(), lst.end());
-	std::cout << big.shortestSpan() << std::endl;
-	std::cout << big.longestSpan() << std::endl;
+	Span big = Span(10001);
+
+	try
+	{
+		std::cout << big.shortestSpan() << std::endl;
+	}
+	catch(const std::exception& e)
+	{
+		std::cerr << e.what() << '\n';
+	}
+
+
+	try
+	{
+		big.addNumber(1000);
+		std::cout << big.shortestSpan() << std::endl;
+	}
+	catch(const std::exception& e)
+	{
+		std::cerr << e.what() << '\n';
+	}
+
+
+	try
+	{
+		big.addNumber(lst.begin(), lst.end());
+		std::cout << big.shortestSpan() << std::endl;
+		std::cout << big.longestSpan() << std::endl;
+	}
+	catch(const std::exception& e)
+	{
+		std::cerr << e.what() << '\n';
+	}
+	try
+	{
+		big.addNumber(42);
+	}
+	catch(const std::exception& e)
+	{
+		std::cerr << e.what() << '\n';
+	}
 
 	try
 	{
