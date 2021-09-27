f53d4043efb3d1812f8388bba01e773234b4fec7
hassansaadfr <saadaoui.hassan@laposte.net>
2021-09-27 19:22:42 +0200

    +

--- cpp06/ex02/srcs/utils.cpp
+++ cpp06/ex02/srcs/utils.cpp
@@ -27,12 +27,35 @@ void	identify(Base *p)
 
 void	identify(Base &p)
 {
-	if (dynamic_cast<A*>(&p))
+	try
+	{
+		A* a = dynamic_cast<A*>(&p);
 		std::cout << "A" << std::endl;
-	else if (dynamic_cast<B*>(&p))
-		std::cout << "B" << std::endl;
-	else if (dynamic_cast<C*>(&p))
+		return ;
+	}
+	catch(const std::exception& e)
+	{
+		std::cerr << e.what() << '\n';
+	}
+	try
+	{
+		B* b = dynamic_cast<B*>(&p);
+		std::cout << "b" << std::endl;
+		return ;
+	}
+	catch(const std::exception& e)
+	{
+		std::cerr << e.what() << '\n';
+	}
+	try
+	{
+		C* c = dynamic_cast<C*>(&p);
 		std::cout << "C" << std::endl;
-	else
-		std::cout << "Cant identify type" << std::endl;
+		return ;
+	}
+	catch(const std::exception& e)
+	{
+		std::cerr << e.what() << '\n';
+	}
+	std::cout << "Cant identify type" << std::endl;
 }
