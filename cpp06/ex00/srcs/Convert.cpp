157669b5fdc4101b8237093c088eb55981dbef78
hassansaadfr <saadaoui.hassan@laposte.net>
2021-09-27 14:03:03 +0200

    ++

--- cpp06/ex00/srcs/Convert.cpp
+++ cpp06/ex00/srcs/Convert.cpp
@@ -1,12 +1,84 @@
 #include "Convert.hpp"
 #include <limits>
 #include <math.h>
+#include <string.h>
+
+int		get_nb_char(std::string s, char to_find)
+{
+	size_t	len = s.length();
+	int		num = 0;
+
+	for (size_t i = 0; i < len; i++)
+	{
+		if (s[i] == to_find)
+			num++;
+	}
+	return num;
+}
+
+double parse_string(char* s)
+{
+	size_t	len = strlen(s);
+
+	if (len == 1)
+	{
+		if (isdigit(s[0]))
+			return atof(s);
+		return s[0];
+	}
+	else if (len > 1)
+	{
+		std::string	str(s);
+		int			dot = get_nb_char(str, '.');
+		int			sign = get_nb_char(str, '-') + get_nb_char(str, '+');
+		int			f = get_nb_char(str, 'f');
+
+		if (dot > 1 || sign > 1 || f > 1)
+			throw std::exception();
+		dot = 0;
+		size_t len = str.length();
+
+		for (size_t i = 0; i < len; i++)
+		{
+			if (strchr("+-.f0123456789", s[i]) == NULL)
+				throw std::exception();
+			else if ((s[i] == '+' || s[i] == '-') && i != 0)
+				throw std::exception();
+			else if (s[i] == '.')
+			{
+				if (i == 0 || i == len - 1 || dot != 0)
+					throw std::exception();
+				if (!isdigit(s[i - 1]) || !isdigit(s[i + 1]))
+					throw std::exception();
+				dot++;
+			}
+			else if (s[i] == 'f')
+			{
+				if (i != len - 1)
+					throw std::exception();
+			}
+		}
+		return atof(s);
+	}
+	else
+		throw std::exception();
+}
 
 Convert::Convert(void)
 {}
 
-Convert::Convert(char *value) :_value(atof(value))
-{}
+Convert::Convert(char *value)
+{
+	std::string tmp = value;
+	if (tmp == "nan" || tmp == "inf" || tmp == "+inf" || tmp == "-inf"
+		|| tmp == "nanf" || tmp == "inff" || tmp == "+inff" || tmp == "-inff"
+	)
+	{
+		_value = atof(value);
+	}
+	else
+		_value = parse_string(value);
+}
 
 Convert::Convert(Convert const &src): _value(src.get_value())
 {}
@@ -54,8 +126,11 @@ int				Convert::to_int(void) const
 
 float			Convert::to_float(void) const
 {
-	if (_value < std::numeric_limits<float>::min() || _value > std::numeric_limits<float>::max())
-		throw Convert::impossibleCast();
+	if (_value < -std::numeric_limits<float>::max() - 1 || _value > std::numeric_limits<float>::max())
+	{
+		if (_value != std::numeric_limits<float>::infinity() && _value != -std::numeric_limits<float>::infinity())
+			throw Convert::impossibleCast();
+	}
 	return static_cast<float>(_value);
 }
 
--- cpp06/ex00/srcs/main.cpp
+++ cpp06/ex00/srcs/main.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include "Convert.hpp"
 #include <iomanip>
+#include <limits>
+#include <math.h>
 
 int main(int ac, char **av)
 {
@@ -48,13 +50,30 @@ int main(int ac, char **av)
 	std::cout << "to float: ";
 	try
 	{
-		std::cout << std::fixed << std::setprecision(1) << to_convert.to_float() << std::endl;
+		float f = to_convert.to_float();
+
+		if ((f > std::numeric_limits<float>::max() || f > -std::numeric_limits<float>::max() - 1) && std::string(av[1]).length() > 5)
+			std::cout << "impossible" << std::endl;
+		else
+		{
+			if (f == std::numeric_limits<float>::infinity())
+				std::cout << "+";
+			std::cout << std::fixed << std::setprecision(1) << f << "f" << std::endl;
+		}
 	}
 	catch(const std::exception& e)
 	{
 		std::cout  << e.what() << std::endl;
 	}
 	std::cout << "to double: ";
-	std::cout << to_convert.to_double() << std::endl;
+	double d = to_convert.to_double();
+	if ((d > std::numeric_limits<double>::max() || d < -std::numeric_limits<double>::max() - 1) && std::string(av[1]).length() > 5)
+		std::cout << "impossible" << std::endl;
+	else
+	{
+		if (d == std::numeric_limits<double>::infinity())
+			std::cout << "+";
+		std::cout << d << std::endl;
+	}
 	return 0;
 }
